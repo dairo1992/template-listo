ec07e1bd6d758d11d1e1575b1e1eb9d1bac32d73
Dairo Barrios <dairo.barrios.ramos@gmail.com>
2024-10-16 10:28:22 -0500

    s

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,12 @@
 export const environment = {
     production: false,
 };
-export const url = 'https://atlas-api.ingdairo.com';
-
+// PRODUCTION
+// export const url = 'https://atlas-api.ingdairo.com';
+// DEVELOPER
+// export const url = 'http://localhost:5500';
+// API NEST
+export const url = 'http://localhost:3000';
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
